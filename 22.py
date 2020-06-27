ac46cca8aeea11d11d16a8fbc9aafec97bb3b1e6
Сухачева Валерия <31817397+Sukhacheva@users.noreply.github.com>
2020-06-27 21:49:30 +0300

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,27 @@
+from datetime import datetime, date, time
+
+def wrapper(f):
+    def writeToFile(*args):
+        #functools
+        writeToFile.__doc__ = f.__doc__
+        print(f.__name__, args)
+        with open('text.txt', "a") as text:
+            time = datetime.utcnow()
+            str = f'{f.__name__}, {args}, {time}'
+            text.write(str)
+            text.write("\r\n")
+        return f(*args)
+    return writeToFile
+
+@wrapper #wrapper(calc)
+def calc(a, b, operation):
+    if (operation == '*'):
+        return '%f * %f = %f' % (a, b, a*b)
+    elif (operation == '/'):
+        return '%f / %f = %f' % (a, b, a/b)
+    elif (operation == '+'):
+        return '%f + %f = %f' % (a, b, a+b)
+    elif (operation == '-'):
+        return '%f - %f = %f' % (a, b, a-b)
+
+print(calc(int(input()),int(input()),str(input())))
